9a901970682e84870e85e6b7e7b4dd9e313e0d5e
vagrant <you@example.com>
2019-03-04 07:12:54 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,289 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table_experiments as dt
+import pandas as pd
+import numpy as np
+from pubmed_lookup import PubMedLookup
+from pubmed_lookup import Publication
+import time
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
+                        'https://codepen.io/chriddyp/pen/brPBPO.css']
+
+semmed = pd.read_csv('./assets/SEMMEDDB_TRIPLES_FINAL.csv')
+pkinfam = pd.read_csv('./assets/pkinfam.tsv', sep='\t')
+results = pd.read_csv('./assets/RESULTS-FINAL-SAMPLE.csv', sep='\t',
+                      usecols=['ProteinKinase_ID', 'KinaseLabel', 'ProteinSubstrate_ID', 'SubstrateLabel', 'Site', 'Score'])
+email = 'raoul.biagioni@ie.fujitsu.com'
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.config['suppress_callback_exceptions']=True
+
+app.layout = html.Div([
+    html.H4('LinkPhinder demo'),
+
+    html.Div([
+        html.Div([
+            # 'A',
+
+            dcc.Dropdown(
+                # value='Enter a value...',
+                options = [],
+                id='dropdown-selected'
+            ),
+            html.Div(id='output'),
+
+            dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
+                    dcc.Tab(label='Kinase View', value='tab-1-example'),
+                    dcc.Tab(label='Substrate View', value='tab-2-example'),
+            ]),
+            html.Div(id='tabs-content-example'),
+
+        ], className='six columns'),
+
+        html.Div([
+            html.Div([
+                html.P('Phosphorylation Sites', style={'font-weight': 'bold',
+                                                       'background-color': 'rgb(112, 205, 130)'}),
+                html.Div(id='sites'),
+                html.Br(),
+            ]),
+
+            # semmedDB
+            html.P('Contextual Information from SemmedDB', style={'font-weight': 'bold',
+                                                                  'background-color': 'rgb(112, 205, 130)'}),
+            html.Div(id='tabs-content-example-right'),
+            html.Div(id='articles'),
+            html.Br(),
+        ], className='six columns'),
+
+    ], className='row'),
+
+], className='ten columns offset-by-one')
+
+
+@app.callback(
+    Output('dropdown-selected', 'options'),
+    [Input('dropdown-selected', 'value')])
+def set_options_1_rel_left(subject):
+    options = [{'label': i, 'value': i} for i in sorted(results['KinaseLabel'].unique().tolist())]
+    return options
+
+@app.callback(Output('tabs-content-example', 'children'),
+              [Input('tabs-example', 'value'),
+               Input('dropdown-selected', 'value')])
+def render_content(tab, entity=None):
+    if tab == 'tab-1-example':
+        rows, related = get_rows(entity, 1)
+        if entity=='' or entity is None:
+            return html.Div([
+                html.P('Please Select a Gene Name')
+                ])
+        else:
+            return html.Div([
+                html.H4('{} as Kinase'.format(entity)),
+                html.Div([
+                    dt.DataTable(
+                        rows = rows,
+                        columns=['KinaseLabel', 'SubstrateLabel', 'Score Range'],
+                        row_selectable=True,
+                        filterable=True,
+                        sortable=True,
+                        selected_row_indices=[],
+                        editable=False,
+                        id='tbl')
+                ]),
+
+        ])
+    elif tab == 'tab-2-example':
+        rows, related = get_rows(entity, 2)
+        return html.Div([
+            html.H4('{} as Substrate'.format(entity)),
+            html.Div([
+                dt.DataTable(
+                    rows=rows,  # df_subj.to_dict('records'),
+                    columns=['KinaseLabel', 'SubstrateLabel', 'Score Range'],
+                    row_selectable=True,
+                    filterable=True,
+                    sortable=True,
+                    selected_row_indices=[],
+                    editable=False,
+                    id='tbl')
+            ]),
+        ])
+
+
+@app.callback(Output('tabs-content-example-right', 'children'),
+              [Input('tabs-example', 'value'),
+               Input('dropdown-selected', 'value')])
+def render_content_right(tab, entity):
+    print(tab)
+
+    if tab == 'tab-1-example':
+        _, related = get_rows(entity, 1)
+        rows, articles = get_semmed(entity, related, 1)
+        time.sleep(2)
+        if len(rows) == 0:
+            if entity is None:
+                return html.P('')
+            else:
+                return html.P('No contextual information available for {}'.format(entity))
+        else:
+            return html.Div([
+                html.Div([
+                    dt.DataTable(
+                        rows=rows,
+                        columns=['SUBJECT', 'RELATION', 'OBJECT', 'PMID'],
+                        row_selectable=False,
+                        filterable=False,
+                        sortable=True,
+                        selected_row_indices=[],
+                        editable=False,
+                        id='tbl3')
+                ]),
+                html.Br(),
+                html.Div(get_article_data(articles))
+            ], style={'margin-top':30})
+    elif tab == 'tab-2-example':
+        _, related = get_rows(entity, 2)
+        rows, articles = get_semmed(entity, related, 2)
+        time.sleep(2)
+        if len(rows) == 0:
+            if entity is None:
+                return html.P('')
+            else:
+                return html.P('No contextual information available for {}'.format(entity))
+        else:
+            return html.Div([
+                html.Div([
+                    dt.DataTable(
+                        rows=rows,
+                        columns=['SUBJECT', 'RELATION', 'OBJECT', 'PMID'],
+                        row_selectable=False,
+                        filterable=False,
+                        sortable=True,
+                        selected_row_indices=[],
+                        editable=False,
+                        id='tbl4')
+                ]),
+                html.Br(),
+                html.Div(get_article_data(articles))
+            ], style={'margin-top':30})
+
+
+
+
+@app.callback(Output('sites', 'children'),
+              [Input('tbl', 'id'),
+               Input('tbl', 'rows'),
+               Input('tbl', 'selected_row_indices')
+               ])
+def show_sites_1(id, rows, selected_row_indices):
+    if len(selected_row_indices) == 0:
+        return html.P('Please select a row in main table on the left.')
+    else:
+        selected_rows = [rows[i] for i in selected_row_indices]
+        rows =  get_site_rows(selected_rows)
+        return dt.DataTable(rows=rows,
+                            columns=['KinaseLabel', 'SubstrateLabel', 'Site', 'Score'],
+                            row_selectable=False,
+                            filterable=False,
+                            sortable=True,
+                            selected_row_indices=[],
+                            editable=False)
+
+
+
+
+def get_site_rows(selected_rows):
+    kinase, substrate = '', ''
+    df_aux = results.copy()
+    for row in selected_rows:
+        for k, v in row.items():
+            if k == 'KinaseLabel':
+                kinase = v
+            if k == 'SubstrateLabel':
+                substrate = v
+        print(kinase, substrate)
+        # if id == 'tbl1':
+        df_aux = df_aux.loc[(df_aux['KinaseLabel'] == kinase) & (df_aux['SubstrateLabel'] == substrate)]
+        df_aux['Score'] = df_aux['Score'].apply(lambda x: np.round(x, 3))
+        rows = df_aux[['KinaseLabel', 'SubstrateLabel', 'Site', 'Score']].to_dict('records')
+        return rows
+
+
+
+
+def get_rows(entity, tab):
+    if entity is None:
+        return [], []
+    else:
+        if tab==1:
+            df1 = results.loc[results['KinaseLabel'] == entity].copy()
+        elif tab==2:
+            df1 = results.loc[results['SubstrateLabel'] == entity].copy()
+
+        df1['Score'] = df1['Score'].apply(lambda x: np.round(x, 3))
+        grouped1 = df1.groupby(['KinaseLabel', 'SubstrateLabel']).agg({'Score': ['min', 'max']})
+        grouped1.columns = grouped1.columns.map('_'.join)
+        grouped1 = grouped1.sort_values(by='Score_min', ascending=False).reset_index()
+        grouped1['Score Range'] = grouped1.apply(lambda row: str(row['Score_min']) + ' - ' + str(row['Score_max']), axis=1)
+        grouped1.drop(['Score_min', 'Score_max'], axis=1, inplace=True)
+        grouped1 = grouped1.reset_index()
+        if len(grouped1) >= 0:
+            rows = grouped1.to_dict('records')
+            if tab==1:
+                related = grouped1.SubstrateLabel.unique().tolist()
+            elif tab==2:
+                related = grouped1.KinaseLabel.unique().tolist()
+            return rows, related
+        else:
+            return [], []
+
+
+
+
+def get_semmed(entity, related, tab):
+    df_subj = semmed.copy()
+
+    if tab == 1:
+        df_subj = df_subj[(df_subj.SUBJECT == entity) & (df_subj.OBJECT.isin(related))]
+    elif tab==2:
+        df_subj = df_subj[(df_subj.OBJECT == entity) & (df_subj.SUBJECT.isin(related))]
+
+    df_subj = df_subj[['SUBJECT', 'RELATION', 'OBJECT', 'PMID']]
+    # df_subj['PMID'] = df_subj['PMID'].apply(lambda x: x.split(','))
+    # df_subj['PMID'] = df_subj['PMID'].apply(lambda x: len(x))
+    # df_subj = df_subj[['SUBJECT', 'RELATION', 'OBJECT', 'PMID_Count']]  # , 'PMID']]
+    # items_subj = sorted(df_subj['SUBJECT'].unique().tolist())
+    rows = df_subj.to_dict('records')
+    articles = df_subj.PMID.tolist()
+    return rows, articles
+
+def get_article_data(articles):
+    grid_layout = []
+    if len(articles) > 0:
+        for article in articles:
+            print(article)
+            pubmedid = article
+            url = 'http://www.ncbi.nlm.nih.gov/pubmed/'+str(pubmedid)
+            print('url', url)
+            lookup = PubMedLookup(url, email)
+            try:
+                publication = Publication(lookup)
+            except:
+                continue
+            grid_layout.append(grid_row(pubmedid, publication.title, url))
+    return grid_layout
+
+def grid_row(pubmedid, title, url):
+    return html.Div([
+        html.P('\n\nArticle {}'.format(pubmedid), style={'font-weight': 'bold'}),
+        html.A(title, href=url, target="_blank")
+    ], style={'padding-top': '0.9em', 'padding-bottom': '0.4em'})
+
+if __name__ == '__main__':
+    # app.run_server(debug=True)
+    app.run_server(debug=True, host = '0.0.0.0', port = 8080)
---
+++ app_v0.py
@@ -0,0 +1,379 @@
+# https://plot.ly/python/reference/
+# https://dash.plot.ly/interactive-graphing
+import json
+from textwrap import dedent as d
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table_experiments as dt
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import networkx as nx
+import pandas as pd
+import numpy as np
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+pkinfam = pd.read_csv('./assets/pkinfam.tsv', sep='\t')
+results = pd.read_csv('./assets/RESULTS-FINAL-SAMPLE.csv', sep='\t',
+                      usecols=['ProteinKinase_ID', 'KinaseLabel', 'ProteinSubstrate_ID', 'SubstrateLabel', 'Site', 'Score'])
+
+print(pkinfam.head())
+print(results.head())
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+#  Layouts
+layout_table = dict(
+    autosize=True,
+    height=500,
+    font=dict(color="#191A1A"),
+    titlefont=dict(color="#191A1A", size='14'),
+    margin=dict(
+        l=35,
+        r=35,
+        b=35,
+        t=45
+    ),
+    hovermode="closest",
+    plot_bgcolor='#fffcfc',
+    paper_bgcolor='#fffcfc',
+    legend=dict(font=dict(size=10), orientation='h'),
+)
+
+G=nx.random_geometric_graph(10,0.125)
+
+G = nx.Graph() # undirected!
+G.add_edge('S', 'B', capacity=10)
+G.add_edge('S', 'T', capacity=4)
+G.add_edge('S', 'M', capacity=5)
+G.add_edge('B', 'M', capacity=8)
+G.add_edge('T', 'M', capacity=3)
+G.add_edge('B', 'R', capacity=6)
+G.add_edge('B', 'O', capacity=9)
+G.add_edge('M', 'R', capacity=15)
+G.add_edge('M', 'O', capacity=12)
+G.add_edge('R', 'O', capacity=4)
+
+fixed_positions = {'S':(1,2.5), 'B':(2.5,5), 'T':(3,1),
+                   'M':(4.5,3), 'R':(7.5,5), 'O':(8.5,3)}
+fixed_nodes = fixed_positions.keys()
+pos = nx.spring_layout(G, pos=fixed_positions, fixed=fixed_nodes)
+
+
+# pos=nx.get_node_attributes(G,'pos')
+dmin=1
+# ncenter=0
+ncenter='S'
+for n in pos:
+    x,y=pos[n]
+    d=(x-0.5)**2+(y-0.5)**2
+    if d<dmin:
+        ncenter=n
+        dmin=d
+
+p=nx.single_source_shortest_path_length(G,ncenter)
+
+edge_trace = go.Scatter(
+    x=[],
+    y=[],
+    line=dict(width=0.5,color='#888'),
+    hoverinfo='none',
+    mode='lines')
+
+for edge in G.edges():
+    for node in edge:
+        x0, y0 = pos[node][0], pos[node][1]
+        # x1, y1 = # G.node[edge[1]]['pos']
+        edge_trace['x'] += tuple([x0])
+        edge_trace['y'] += tuple([y0])
+
+
+
+node_trace = go.Scatter(
+    x=[],
+    y=[],
+    text=[],
+    textposition='bottom center',
+    mode='markers',
+    hoverinfo='text',
+    marker=dict(
+        showscale=False,
+        # colorscale options
+        #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+        #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+        #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+        colorscale='YlGnBu',
+        reversescale=True,
+        color=[],
+        size=10,
+        colorbar=dict(
+            thickness=15,
+            title='Node Connections',
+            xanchor='left',
+            titleside='right'
+        ),
+        line=dict(width=2)))
+
+for node in G.nodes():
+    x, y = pos[node][0], pos[node][1]
+    node_trace['x'] += tuple([x])
+    node_trace['y'] += tuple([y])
+    # node_trace['text'] += tuple([node])
+
+# https://community.plot.ly/t/markers-text-mode-is-it-possible-to-have-different-text-and-hover-text/14656/4
+annotations = []
+for node, adjacencies in enumerate(G.adjacency()):
+    node_trace['marker']['color']+=tuple([len(adjacencies[1])])
+    node_info = '# of connections: '+str(len(adjacencies[1]))
+    node_trace['text'] += tuple(['AKT1' ])#tuple([node_info])
+
+    annotations.append(
+        dict(x=pos[adjacencies[0]][0],
+             y=pos[adjacencies[0]][1],
+             text=adjacencies[0],  # node name that will be displayed
+             xanchor='left',
+             xshift=10,
+             font=dict(color='black', size=10),
+             showarrow=False, arrowhead=1, ax=-10, ay=-10)
+    )
+
+# fig = go.Figure(data=[edge_trace, node_trace],
+#              layout=go.Layout(
+#                 title='<br>Network graph made with Python',
+#                 titlefont=dict(size=16),
+#                 showlegend=False,
+#                 hovermode='closest',
+#                 margin=dict(b=20,l=5,r=5,t=40),
+#                 annotations=[ dict(
+#                     text="Python code: <a href='https://plot.ly/ipython-notebooks/network-graphs/'> https://plot.ly/ipython-notebooks/network-graphs/</a>",
+#                     showarrow=False,
+#                     xref="paper", yref="paper",
+#                     x=0.005, y=-0.002 ) ],
+#                 xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+#                 yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
+#                 ), # end layout
+#                 ) # end Figure
+
+
+
+app.layout = html.Div([
+
+    # html.Div([
+    #
+    #
+    # dcc.Graph(
+    #     id='basic-interactions',
+    #     figure={
+    #         'data': [
+    #             {
+    #                 'x': [1, 2, 3, 4],
+    #                 'y': [4, 1, 3, 5],
+    #                 'text': ['a', 'b', 'c', 'd'],
+    #                 'customdata': ['c.a', 'c.b', 'c.c', 'c.d'],
+    #                 'name': 'Trace 1',
+    #                 'mode': 'markers',
+    #                 'marker': {'size': 12}
+    #             },
+    #             {
+    #                 'x': [1, 2, 3, 4],
+    #                 'y': [9, 4, 1, 4],
+    #                 'text': ['w', 'x', 'y', 'z'],
+    #                 'customdata': ['c.w', 'c.x', 'c.y', 'c.z'],
+    #                 'name': 'Trace 2',
+    #                 'mode': 'markers',
+    #                 'marker': {'size': 12}
+    #             }
+    #         ],
+    #
+    #         'layout': {
+    #             'clickmode': 'event+select'
+    #         }
+    #     }
+    # ),
+    # ], style={'width': '49%', 'display': 'inline-block'}),
+    #
+    # html.Div(className='row', children=[
+    #
+    #         dcc.Markdown("""
+    #             **Click Data**
+    #             Click on points in the graph.
+    #         """),
+    #         html.Pre(id='click-data', style=styles['pre']),
+    #
+    # ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'}),
+
+    html.Div([
+
+        # https://plot.ly/python/user-guide/
+        # https://github.com/jimmybow/visdcc/blob/master/README.md#3-animate-or-move-the-camera-
+        dcc.Graph(id='basic-interactions2',
+                  figure={
+                      'data': [edge_trace, node_trace],
+
+                      'layout':
+                          {
+                           'title': 'Dash Data Visualization',
+                           'titlefont' : dict(size=14),
+                           'showlegend': False,
+                           'xaxis': dict(showgrid=False, zeroline=False, showticklabels=False),
+                           'yaxis': dict(showgrid=False, zeroline=False, showticklabels=False),
+                           'annotations': annotations,
+                           'clickmode': 'event+select'
+                          }
+                  }),
+
+    ], style={'width': '49%', 'display': 'inline-block'}),
+
+    html.Div(className='row', children=[
+
+        dcc.Markdown("""
+            **Click Data**
+            Click on points in the graph.
+        """),
+        html.Pre(id='click-data2', style=styles['pre']),
+
+    ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'}),
+
+
+    html.Div([
+
+        dcc.Dropdown(
+            value='Enter a value...',
+            options = [{'label': i, 'value': i} for i in pkinfam['kinase_name'].unique().tolist()],
+            id='input'
+        ),
+
+    ], className='row'),
+
+    html.Div(id='output'),
+
+
+    html.Div([
+        html.Div([
+        dt.DataTable(
+                rows=[],  # df_subj.to_dict('records'),
+                columns=['KinaseLabel', 'SubstrateLabel', 'Score Range'],
+                row_selectable=False,
+                filterable=True,
+                sortable=True,
+                selected_row_indices=[],
+                editable=False,
+                id='click-data3')
+        ], style = layout_table, className='six columns'),
+
+        html.Div([
+        dt.DataTable(
+                rows=[],  # df_subj.to_dict('records'),
+                columns=['KinaseLabel', 'SubstrateLabel', 'Score Range'],
+                row_selectable=False,
+                filterable=True,
+                sortable=True,
+                selected_row_indices=[],
+                editable=False,
+                id='click-data4')
+        ], style = layout_table, className='six columns'),
+
+    ], className='row'),
+
+
+])
+
+# @app.callback(
+#     Output('hover-data', 'children'),
+#     [Input('basic-interactions', 'hoverData')])
+# def display_hover_data(hoverData):
+#     return json.dumps(hoverData, indent=2)
+
+
+# @app.callback(
+#     Output('click-data', 'children'),
+#     [Input('basic-interactions', 'clickData')])
+# def display_click_data(clickData):
+#     return json.dumps(clickData, indent=2)
+
+
+@app.callback(Output('output', 'children'),
+              [Input('input', 'value')])
+def update_output(value):
+        return value
+
+
+
+
+@app.callback(
+    Output('click-data2', 'children'),
+    [Input('basic-interactions2', 'clickData')])
+def display_click_data_2(clickData):
+
+    print(clickData['points'][0]['text'])
+    return json.dumps(clickData, indent=2)
+
+# Table
+@app.callback(
+    Output('click-data3', 'rows'),
+    # [Input('basic-interactions2', 'clickData')])
+    [Input('input', 'value')])
+def display_click_data_3(value):
+
+    # filter = clickData['points'][0]['text']
+
+    df1 = results.loc[results['KinaseLabel'] == value].copy()
+    df1['Score'] = df1['Score'].apply(lambda x: np.round(x, 3))
+    grouped1 = df1.groupby(['KinaseLabel', 'SubstrateLabel']).agg({'Score': ['min', 'max']})
+    grouped1.columns = grouped1.columns.map('_'.join)
+    grouped1 = grouped1.sort_values(by='Score_min', ascending=False).reset_index()
+    grouped1['Score Range'] = grouped1.apply(lambda row: str(row['Score_min']) + ' - ' + str(row['Score_max']), axis=1)
+    grouped1.drop(['Score_min', 'Score_max'], axis=1, inplace=True)
+    grouped1 = grouped1.reset_index()
+
+    # df_aux = df_aux.sort_values(by='PMID_Count', ascending=False)
+    rows = grouped1.to_dict('records')
+    return rows
+
+@app.callback(
+    Output('click-data4', 'rows'),
+    # [Input('basic-interactions2', 'clickData')])
+    [Input('input', 'value')])
+def display_click_data_4(value):
+
+    # filter = clickData['points'][0]['text']
+    df2 = results.loc[results['SubstrateLabel'] == value].copy()
+
+    df2['Score'] = df2['Score'].apply(lambda x: np.round(x, 3))
+    grouped2 = df2.groupby(['KinaseLabel', 'SubstrateLabel']).agg({'Score': ['min', 'max']})
+    grouped2.columns = grouped2.columns.map('_'.join)
+    grouped2 = grouped2.sort_values(by='Score_min', ascending=False).reset_index()
+    grouped2['Score Range'] = grouped2.apply(lambda row: str(row['Score_min']) + ' - ' + str(row['Score_max']), axis=1)
+    grouped2.drop(['Score_min', 'Score_max'], axis=1, inplace=True)
+    grouped2 = grouped2.reset_index()
+    print(grouped2.head())
+    # df_aux = df_aux.sort_values(by='PMID_Count', ascending=False)
+    rows = grouped2.to_dict('records')
+    return rows
+
+# @app.callback(
+#     Output('selected-data', 'children'),
+#     [Input('basic-interactions', 'selectedData')])
+# def display_selected_data(selectedData):
+#     return json.dumps(selectedData, indent=2)
+#
+#
+# @app.callback(
+#     Output('relayout-data', 'children'),
+#     [Input('basic-interactions', 'relayoutData')])
+# def display_selected_data(relayoutData):
+#     return json.dumps(relayoutData, indent=2)
+
+
+if __name__ == '__main__':
+    # app.run_server(debug=True)
+    app.run_server(host = '0.0.0.0', port = 8080)
